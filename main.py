1e60f8de9f5527e42a1c85b89804569b0dce5faf
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-08-01 23:12:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,3 +31,17 @@ while True:
         score = score + 20
     pygame.draw.rect(display, pole_color, (pole_x, 0, pole_width, top_pole_height))
     pygame.draw.rect(display, pole_color, (pole_x, top_pole_height + pole_gap, pole_width, 568))
+# collision
+    if pole_x <= bird_x + 50 and bird_x <= pole_x + pole_width:
+        # In the video you will see the line below
+        # if bird_y <= top_pole_height or bird_y >= top_pole_height + pole_gap:
+        # However, the bird bottom has to more than the top pole height and gap to
+        # hit the bottom pole.
+        if bird_y <= top_pole_height or bird_y + 50 >= top_pole_height + pole_gap:
+            score = score - 5
+            print('Collision !! Bye Bye bird baby!!!', score)
+
+    score_text = text.render(f'Score: {score}', True, (255, 255, 255))
+    display.blit(score_text, (0, 0))
+    pygame.display.update()
+    clock.tick(60)
