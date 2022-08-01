502748b759cd99b152fb1ec48faccf7a44c70781
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-08-01 23:11:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import pygame
+from random import randint
+pygame.init()
+display = pygame.display.set_mode((320, 568))
+text = pygame.font.Font(None, 50)
+bg = pygame.image.load('bg.png')
+bird = pygame.image.load('bird.png')
+pole_width = 70
+pole_gap = 100
+pole_x = 320
+top_pole_height = randint(100, 400)
+pole_color = (220, 85, 57)
+bird_x = 20
+bird_y = top_pole_height + 20
+score = 0
+clock = pygame.time.Clock()
 
